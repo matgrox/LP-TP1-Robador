7f618475da749bbe61abb4a67f715debebbdb122
Matias R <matiasrob.mer@gmail.com>
2025-04-09 22:46:12 -0300

    feat: nueva funcionalidad de inicio de session agregada

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package demo;
+
+import business.security.AuthenticationService;
 
 /**
  *
@@ -11,6 +12,28 @@ package demo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        pruebaCorrecta();
+        pruebaIncorrecta();
+    }
+
+    public static void pruebaCorrecta() {
+         AuthenticationService a = new AuthenticationService();
+        long verif = a.signin("admin","#123456$");
+        if(verif>0){
+            System.out.println("Credenciales correctas");
+        }
+        else{
+            System.out.println("Credenciales Incorrectas");
+        }
+    }
+    public static void pruebaIncorrecta() {
+        AuthenticationService a = new AuthenticationService();      
+        long verif = a.signin("administrador","#123456$");
+        if(verif>0){
+            System.out.println("Credenciales correctas");
+        }
+        else{
+            System.out.println("Credenciales Incorrectas");
+        }
     }
 }
