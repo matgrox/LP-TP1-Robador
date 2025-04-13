ed97745f2a0f221fb04cadb397946711170c03c1
matgrox <149273859+matgrox@users.noreply.github.com>
2025-04-12 21:34:24 -0300

    Revert "feat: se implementa ventana de inicio de sesión"

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,9 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package demo;
 
-import business.security.AuthenticationService;
+package demo;
 
 /**
  *
@@ -12,29 +11,6 @@ import business.security.AuthenticationService;
 public class Main {
 
     public static void main(String[] args) {
-        //pruebaCorrecta();
-        //pruebaIncorrecta();
-        new gui.FormSignin().setVisible(true);
-    }
-
-    public static void pruebaCorrecta() {
-         AuthenticationService a = new AuthenticationService();
-        long verif = a.signin("admin","#123456$");
-        if(verif>0){
-            System.out.println("Credenciales correctas");
-        }
-        else{
-            System.out.println("Credenciales Incorrectas");
-        }
-    }
-    public static void pruebaIncorrecta() {
-        AuthenticationService a = new AuthenticationService();      
-        long verif = a.signin("administrador","#123456$");
-        if(verif>0){
-            System.out.println("Credenciales correctas");
-        }
-        else{
-            System.out.println("Credenciales Incorrectas");
-        }
+        System.out.println("Hello World!");
     }
 }
